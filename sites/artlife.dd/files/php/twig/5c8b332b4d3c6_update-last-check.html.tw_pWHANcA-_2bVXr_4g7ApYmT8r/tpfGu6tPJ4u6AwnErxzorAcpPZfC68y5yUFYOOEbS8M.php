f07aed371ecc7284ec345cc957ea1dcf4d171562
osdal <osdal67@gmail.com>
2019-03-15 08:23:30 +0200

    8.6.12+admin_tools

---
+++ sites/artlife.dd/files/php/twig/5c8b332b4d3c6_update-last-check.html.tw_pWHANcA-_2bVXr_4g7ApYmT8r/tpfGu6tPJ4u6AwnErxzorAcpPZfC68y5yUFYOOEbS8M.php
@@ -0,0 +1,106 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/stable/templates/admin/update-last-check.html.twig */
+class __TwigTemplate_3f6e4ed7965d76c8d33bbd72018e44c02b2b686defbbb91fdeb11f5bf88cbbc8 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 15];
+        $filters = ["t" => 16];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['t'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 14
+        echo "<p>
+  ";
+        // line 15
+        if (($context["last"] ?? null)) {
+            // line 16
+            echo "    ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Last checked: @time ago", ["@time" => ($context["time"] ?? null)]));
+            echo "
+  ";
+        } else {
+            // line 18
+            echo "    ";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Last checked: never"));
+            echo "
+  ";
+        }
+        // line 20
+        echo "  (";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["link"] ?? null)), "html", null, true);
+        echo ")
+</p>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/stable/templates/admin/update-last-check.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  72 => 20,  66 => 18,  60 => 16,  58 => 15,  55 => 14,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/stable/templates/admin/update-last-check.html.twig", "C:\\Aquia\\artlife\\core\\themes\\stable\\templates\\admin\\update-last-check.html.twig");
+    }
+}
